f5bbe5681c981086ad28ab5832c0ee1f3d508b07
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-02-26 01:28:05 -0800

    added generated_plots to gitignore

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 # plotting functions
 
 import matplotlib.pyplot as plt
+import time
 
 # fibAddsPlot
 #   plots the number of additions from fibonacci.py/addCount
@@ -12,6 +13,7 @@ def fib_adds_plot(addArr):
     plt.plot(addArr, label='add ops', color='red')
     plt.plot(addArr, 'r.')
     plt.legend()
+
     plt.show()
     print("addArr:", addArr)
 
@@ -26,6 +28,7 @@ def gcd_mods_plot(gcdModsArr):
     plt.plot(gcdModsArr, label='mod ops.', color='blue')
     plt.plot(gcdModsArr, 'b.')
     plt.legend()
+
     plt.show()
     print("gcdModsArr:", gcdModsArr)
 
@@ -43,6 +46,12 @@ def exp_plots(dbo_arr, dbf_arr, dac_arr):
     plt.plot(dac_arr, label='Divide and Conquer', color='green')
     plt.plot(dac_arr, 'g.')
     plt.legend()
+
+    tm = time.gmtime()
+    save_name = "Exponent_M_n " + time.strftime("%Y-%m-%d %H:%M:%S", tm) + ".png"
+    # print(save_name)
+    plt.savefig('generated_plots/save')
+
     plt.show()
     print("dec_by_one_plot:", dbo_arr)
     print("dec_by_fctr_plot:", dbf_arr)
